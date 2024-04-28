db83081c62d727dea65d72452ff072cc0f4726db
a1726218 <a1726218@student.adelaide.edu.au>
2024-04-28 22:11:46 +0930

    Completed Question 4 Part 1

--- Q4P1.java
+++ Q4P1.java
@@ -2,6 +2,9 @@ import org.moeaframework.algorithm.NSGAII;
 import org.moeaframework.algorithm.MOEAD;
 import org.moeaframework.core.Problem;
 import org.moeaframework.problem.ZDT.ZDT1;
+import org.moeaframework.problem.ZDT.ZDT2;
+import org.moeaframework.problem.DTLZ.DTLZ3;
+import org.moeaframework.problem.DTLZ.DTLZ4;
 import org.moeaframework.core.NondominatedPopulation;
 import org.moeaframework.core.Solution;
 
@@ -9,22 +12,23 @@ public class Example1 {
 
     public static void main(String[] args) {
         int repetitions = 10;
+        //change population size according to problem
         int populationSize = 20;
         int evaluations = 10000;
-        //initialization of problem
+        double[] overall_average = new double[repetitions];
+        double[] overall_std = new double[repetitions];
+        
+        // Initialization of problem
+        // Change problem according to specifics
         Problem problem = new ZDT1(2);
 
         System.out.println("Problem: " + problem.getName());
-
         System.out.println("Population Size: " + populationSize);
 
         for (int i = 0; i < repetitions; i++) {
-            System.out.println("Repetition: " + (i + 1));
-
             NSGAII nsgaII = new NSGAII(problem);
-            MOEAD moead = new MOEAD(problem);
 
-            // Run NSGA-II
+            // Run algorithm
             nsgaII.run(evaluations);
             NondominatedPopulation nsgaIIPopulation = nsgaII.getResult();
 
@@ -42,7 +46,7 @@ public class Example1 {
             // Calculate the average of all objective values
             double[] averageObjectives = new double[sumObjectives.length];
             for (int k = 0; k < sumObjectives.length; k++) {
-            	averageObjectives[k] = sumObjectives[k] / totalObjectiveValues;
+                averageObjectives[k] = sumObjectives[k] / totalObjectiveValues;
             }
             
             // Calculate the standard deviation of objective values
@@ -58,17 +62,18 @@ public class Example1 {
                 standardDeviations[m] = Math.sqrt(squaredDifferences[m] / populationSize);
             }
 
-            // Print or use the average objective values as needed
-            System.out.println("Average Objective Values:");
-            for (double avg : averageObjectives) {
-                System.out.println(avg);
-            }
-            
-            //Print or use the std deviation values as needed
-            System.out.println("Std Deviation Values: ");
-            for(double std: standardDeviations) {
-            	System.out.println(std);
-            }
+            // Store the average and standard deviation values
+            overall_average[i] = averageObjectives[0]; 
+            overall_std[i] = standardDeviations[0];
+        }
+        System.out.println("Overall Average Objective Values:");
+        for (double avg : overall_average) {
+            System.out.println(avg);
+        }
+        System.out.println("Overall Standard Deviation Values:");
+        for (double std : overall_std) {
+            System.out.println(std);
         }
     }
 }
+
