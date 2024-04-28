cf4fa01eb4bd33ac1de17d06989e41adeb2911a8
ProgrammingBoulderer <100819917+ProgrammingBoulderer@users.noreply.github.com>
2024-04-28 18:54:23 +0930

    Update Q4P1.java
    Added function to calculate average and standard deviation
    
    Added function to create scatter plot

--- Q4P1.java
+++ Q4P1.java
@@ -2,34 +2,73 @@ import org.moeaframework.algorithm.NSGAII;
 import org.moeaframework.algorithm.MOEAD;
 import org.moeaframework.core.Problem;
 import org.moeaframework.problem.ZDT.ZDT1;
-import org.moeaframework.problem.ZDT.ZDT2;
-import org.moeaframework.problem.DTLZ.DTLZ3;
-import org.moeaframework.problem.DTLZ.DTLZ4;
+import org.moeaframework.analysis.plot.Plot;
 
 public class Example1 {
 
     public static void main(String[] args) {
         int repetitions = 10;
         int populationSize = 20;
-        //array of problems
-        Problem problems[] = {new ZDT1(2), new ZDT2(2), new DTLZ3(3), new DTLZ4(3)};
+        int evaluations = 10000;
+        // array of problems
+        Problem problem = new ZDT1();
 
-        for (Problem problem : problems) {
-            //print out results
-            System.out.println("Results for " + problem.getName() + " with population size " + populationSize + " after " + repetitions + " repetitions:");
-            for (int i = 0; i < populationSize; i++) {
-                NSGAII algorithm = new NSGAII(problem);
-                MOEAD algorithm2 = new MOEAD(problem);
+        System.out.println("Problem: " + problem.getName());
 
-                for (int j = 0; j < repetitions; j++) {
-                    algorithm.run(10);
-                    algorithm2.run(10);
-                }
-                System.out.println("NSGAII Results:");
-                algorithm.getResult().display();
-                System.out.println("MOEAD Results:");
-                algorithm2.getResult().display();
-            }
+        System.out.println("Population Size: " + populationSize);
+
+        double[] nsgaIIResults = new double[repetitions];
+        double[] moeadResults = new double[repetitions];
+
+        for (int j = 0; j < repetitions; j++) {
+            System.out.println("Repetition: " + (j + 1));
+
+            NSGAII nsgaII = new NSGAII(problem);
+            MOEAD moead = new MOEAD(problem);
+
+            // Run NSGA-II
+            nsgaII.run(evaluations);
+
+            // Run MOEA/D
+            moead.run(evaluations);
         }
+
+        // Calculate and display average and standard deviation for NSGA-II
+        double nsgaIIAvg = calculateAverage(nsgaIIResults);
+        double nsgaIIStdDev = calculateStandardDeviation(nsgaIIResults);
+        System.out.println("NSGA-II - Average: " + nsgaIIAvg + ", StdDev: " + nsgaIIStdDev);
+
+        // Calculate and display average and standard deviation for MOEA/D
+        double moeadAvg = calculateAverage(moeadResults);
+        double moeadStdDev = calculateStandardDeviation(moeadResults);
+        System.out.println("MOEA/D - Average: " + moeadAvg + ", StdDev: " + moeadStdDev);
+
+        // Plot results
+        Plot plot = new Plot()
+            .scatter("NSGA-II", nsgaIIResults, moeadResults)
+            .setXLabel("Repetition")
+            .setYLabel("Metric Value")
+            .setTitle("Comparison of NSGA-II and MOEA/D");
+
+        plot.show();
+    }
+
+    // Function to calculate the average of an array of doubles
+    private static double calculateAverage(double[] values) {
+        double sum = 0.0;
+        for (double value : values) {
+            sum += value;
+        }
+        return sum / values.length;
+    }
+
+    // Function to calculate the standard deviation of an array of doubles
+    private static double calculateStandardDeviation(double[] values) {
+        double average = calculateAverage(values);
+        double sumSquaredDiff = 0.0;
+        for (double value : values) {
+            sumSquaredDiff += Math.pow(value - average, 2);
+        }
+        return Math.sqrt(sumSquaredDiff / values.length);
     }
 }
